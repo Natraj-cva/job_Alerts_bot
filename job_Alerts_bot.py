9926935cdeda4d25525204eb98667738f7dcbb18
Natraj-cva <vasuunatraj@gmail.com>
2026-04-04 08:37:24 +0530

    Add files via upload

---
+++ job_Alerts_bot.py
@@ -0,0 +1,91 @@
+import time 
+import requests
+from bs4 import BeautifulSoup
+import os
+
+BOT_TOKEN = "8269721212:AAFbpeL2GrKDo-viX4IGZ7LEQ5YavrM-7hE"
+CHAT_ID = "996067981"
+SEEN_FILE = "seen_jobs.txt"
+
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot8269721212:AAFbpeL2GrKDo-viX4IGZ7LEQ5YavrM-7hE/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    print("Telegram response:", response.text)
+
+
+def load_seen_jobs():
+    if not os.path.exists(SEEN_FILE):
+        return set()
+
+    with open(SEEN_FILE, "r", encoding="utf-8") as file:
+        return set(line.strip() for line in file if line.strip())
+
+
+def save_seen_job(job):
+    with open(SEEN_FILE, "a", encoding="utf-8") as file:
+        file.write(job + "\n")
+
+
+def get_remoteok_jobs():
+    url = "https://remoteok.com/remote-python-jobs"
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
+    print("Website status:", response.status_code)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = []
+
+    rows = soup.find_all("tr", class_="job")
+    print("Total job rows found:", len(rows))
+
+    for row in rows[:10]:
+        title_tag = row.find("h2")
+        company_tag = row.find("h3")
+        link_tag = row.find("a", class_="preventLink")
+
+        if title_tag and company_tag:
+            title = title_tag.get_text(strip=True)
+            company = company_tag.get_text(strip=True)
+
+            job_link = ""
+            if link_tag and link_tag.get("href"):
+                job_link = "https://remoteok.com" + link_tag.get("href")
+
+            job_text = f"{title} at {company}"
+            full_job = f"{job_text}\n{job_link}".strip()
+
+            jobs.append(full_job)
+
+    return jobs
+
+while True:
+    print("\n--- Running job check ---")
+    seen_jobs = load_seen_jobs()
+    jobs = get_remoteok_jobs()
+
+    new_jobs = []
+
+    for job in jobs:
+        if job not in seen_jobs:
+            new_jobs.append(job)
+            save_seen_job(job)
+
+    if new_jobs:
+        message = "New Job Alerts:\n\n" + "\n\n".join(new_jobs)
+    else:
+        message = "No new jobs found right now."
+
+    print("Final message:")
+    print(message)
+
+    send_telegram_message(message)
+    print("Sleeping for 10 minutes...\n")
+    time.sleep(600)  # 600 seconds = 10 minutes
